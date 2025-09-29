dbc10bd3d51fe3f2426ce5818653b4f4c9d0feb0
Emanuel <gutierrez.vasquez.emanuel@gmail.com>
2025-09-29 06:37:31 -0400

    Fix Cloudin media storag

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = config('STATIC_URL', default='/static/')
-MEDIA_URL = config('MEDIA_URL', default='/media/')
+# MEDIA_URL = config('MEDIA_URL', default='/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
