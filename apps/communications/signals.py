bcfc0a9e0790d4471b12510b16e586c2a862846f
Emanuel <gutierrez.vasquez.emanuel@gmail.com>
2025-12-17 21:37:39 -0400

    config eleccion serviceaccount desplegado

--- apps/communications/signals.py
+++ apps/communications/signals.py
@@ -1,16 +1,29 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, messaging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Communication # Importa tu modelo de Avisos
+from .models import Communication
 
 # 1. Inicializar Firebase Admin (Hazlo una sola vez, quizás en settings.py es mejor, pero aquí sirve para probar)
 try:
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json") # Ruta a tu JSON
+        # 1. Definimos la ruta del archivo
+        # En Render, los Secret Files se guardan en /etc/secrets/
+        # En local, buscamos en la raíz del proyecto
+        
+        if os.path.exists('/etc/secrets/serviceAccountKey.json'):
+            cred_path = '/etc/secrets/serviceAccountKey.json'  # Ruta de Render
+        else:
+            cred_path = 'serviceAccountKey.json'  # Ruta Local (Windows/Mac)
+
+        # 2. Inicializamos con la ruta detectada
+        cred = credentials.Certificate(cred_path)
         firebase_admin.initialize_app(cred)
+        print(f"✅ Firebase inicializado usando: {cred_path}")
+        
 except Exception as e:
-    print(f"Error init firebase: {e}")
+    print(f"❌ Error iniciando Firebase: {e}")
 
 @receiver(post_save, sender=Communication)
 def send_notification_on_new_notice(sender, instance, created, **kwargs):
